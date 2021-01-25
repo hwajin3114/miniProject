092c169aacb66d8ba68e850234693dd5b2e85ada
LEE HWAJIN <LEE HWAJIN@DESKTOP-4EM9M6Q>
2021-01-25 20:46:27 +0900

    프로젝트 설정 - tiles 적용

---
+++ jamong/src/main/java/co/jamong/controller/HomeController.java
@@ -0,0 +1,23 @@
+package co.jamong.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
+
+	@RequestMapping(value="/admin")
+	public ModelAndView admin(HttpServletResponse response) throws IOException{
+		return new ModelAndView("admin/dashboard");
+	}
+}
